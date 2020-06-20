b80c6b3da7028ebed9e0d4d7123da0f11d32ccfe
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-20 16:25:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,61 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_data = {
+
+  :color => {
+
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+
+  :gender => {
+
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+
+  :lives => {
+
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+
+def pigeon_helper(data)
+
+  final_hash = Hash.new
+
+  data.each do |attribute, subattr|
+
+    subattr.each do |subattr, name|
+      
+      name.each do |name|
+
+        if final_hash.include? name
+
+          str_subattr = subattr.to_s
+          final_hash[name][attribute] << str_subattr
+
+        else
+          
+          final_hash[name] = name
+          str_subattr = subattr.to_s
+          final_hash[name][attribute]
+
+      end
+
+    end
+
+  end
+  p final_hash
+
+end
+
+    
+pigeon_helper(pigeon_data)
+end 
 end
